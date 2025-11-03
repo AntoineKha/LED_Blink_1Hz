79ed33ad472ac86255fc7ee30164b80769abac66
AntoineKha <Antoine_khayat@outlook.com>
2025-11-03 18:02:12 -0500

    Updated comments
    Updated comments for clarity

--- LED_Blink_1Hz_tb.v
+++ LED_Blink_1Hz_tb.v
@@ -14,21 +14,22 @@ module LED_Blink_1Hz_tb;
     );
     
   // Generate the clock according to our actual period (8ns).
-    always #4 clk = ~clk;
+    always #4 clk = ~clk;    // Every 4 ns
     
-    // Run the test
-    initial begin
-        clk = 0;
+    // Begin with 0 as values for reset and clock.
+    initial begin    
+        clk = 0;    
         rst_btn = 0;
         
       // Reset assertion so that our count can begin.
-        #100;
+        #100;    // after 100 ns
         rst_btn = 1;
-        #100;
+        #100;    // after another 100ns (200ns total)
         rst_btn = 0;
         
       // We will run it for 0.505s, can be ommitted in the case where simulation time is controlled by settings (Like in Vivado). 
-      // #505_000_000
+       #505_000_000;
+        $finish;
     end
     
 endmodule
